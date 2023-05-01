d3050b4d29c5bd2c286583c412cd71be450fb154
mengxianghan <1056811341@qq.com>
2023-05-01 17:00:40 +0800

    chore: 调整构建目录和文件名

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,28 @@
 import { terser } from 'rollup-plugin-terser'
-
 import babel from '@rollup/plugin-babel'
 import cleaner from 'rollup-plugin-cleaner'
 
 export default {
-    input: 'src/main.js',
+    input: 'src/index.js',
     output: [
         {
-            file: 'dist/index.esm.js',
+            file: 'dist/es/index.js',
             format: 'esm',
         },
         {
-            file: 'dist/index.esm.min.js',
+            file: 'dist/es/index.min.js',
             format: 'esm',
             plugins: [terser()],
         },
         {
-            file: 'dist/index.umd.js',
+            file: 'dist/index.js',
             format: 'umd',
-            name: 'Enum',
+            name: 'XYEnum',
         },
         {
-            file: 'dist/index.umd.min.js',
+            file: 'dist/index.min.js',
             format: 'umd',
-            name: 'Enum',
+            name: 'XYEnum',
             plugins: [terser()],
         },
     ],
